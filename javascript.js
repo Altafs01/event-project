a5d3700cd2a84ec897388f07bb18aa4093d8887d
Altafs01 <altafshaikh1697@gmail.com>
2023-11-06 00:23:40 +0530

    changes i have made

---
+++ javascript.js
@@ -0,0 +1,18 @@
+var a = document.querySelector("body")
+
+var b = document.querySelector("svg")
+
+var flag = 0;
+
+a.addEventListener("click", function(){
+    if (flag == 0){
+        a.style.backgroundColor = "black"
+        a.style.color = "white"
+        b.style.color = "black"
+        flag = 1
+    }else{
+        a.style.backgroundColor = "transparent"
+        a.style.color = "black"
+        flag = 0
+    }
+})
