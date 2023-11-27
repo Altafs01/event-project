89d6f823204d8d37022eeaeee34b56c04ceca2d2
Altafs01 <altafshaikh1697@gmail.com>
2023-11-28 00:16:27 +0530

    changes i have made

--- javascript.js
+++ javascript.js
@@ -2,6 +2,8 @@ var a = document.querySelector("body")
 
 var b = document.querySelector("svg")
 
+var c = document.querySelector("ul")
+
 var flag = 0;
 
 b.addEventListener("click", function(){
@@ -9,10 +11,13 @@ b.addEventListener("click", function(){
         a.style.backgroundColor = "black"
         a.style.color = "white"
         b.style.color = "black"
+        // c.style.backgroundColor = "black"
+        c.style.color = "white"
         flag = 1
     }else{
         a.style.backgroundColor = "transparent"
         a.style.color = "black"
+        // c.style.color = "white"
         flag = 0
     }
 })
